33ab1826ffd233a8b9ffbadef0da86b82f7f91a4
Aleksandra Nitka <ola@trident-ww.com>
2021-06-07 23:45:08 +0200

    Add activate coupon logic

--- couponsltd-web/src/components/CouponPage/Coupon.jsx
+++ couponsltd-web/src/components/CouponPage/Coupon.jsx
@@ -5,6 +5,7 @@ import AppConfig from './../../config';
 export default function Coupons(props) {
   const [coupons, setCoupons] = useState(null);
   const [searchText, setSearchText] = useState('');
+  const [promoCodes, setPromoCodes] = useState({});
 
   useEffect(() => {
     axios
@@ -21,36 +22,76 @@ export default function Coupons(props) {
       .catch(console.log);
   }, [searchText]);
 
+  const handlePromoCode = (id, code) => {
+    let codes = { ...promoCodes };
+    codes[id] = code;
+    setPromoCodes(codes);
+  };
+
+  async function handleActivate(couponId, e) {
+    e.preventDefault();
+
+    let promoCode = promoCodes[couponId];
+    axios
+      .post(
+        `${AppConfig.ApiRootUrl}coupons/activatebonus/${couponId}/${promoCode}`,
+        { text: searchText, skip: 0, limit: 10, orderBy: 0 },
+        {
+          headers: { Authorization: `Bearer ${props.token.token}` },
+        }
+      )
+      .then((response) => {
+        let newCoupons = [...coupons];
+        var activatedArr = newCoupons.map((c) => {
+          var coupon = { ...c };
+          if (c.id === couponId) {
+            coupon.isActived = true;
+          }
+          return coupon;
+        });
+
+        setCoupons(activatedArr);
+      })
+      .catch(console.log);
+  }
+
   let couponsList = coupons?.map((c, index) => {
     return (
-      <div class='card mt-4' key={index}>
-        <div class='card-body'>
+      <div className='card mt-4' key={c.id}>
+        <div className='card-body'>
           <div className='row'>
             <div className='col-4'>
               <h5>{c.name}</h5>
               <p>{c.description}</p>
             </div>
             <div className='col-8 d-flex flex-column justify-content-center'>
-              <form class='form-inline d-flex'>
+              <form className='form-inline d-flex'>
                 {!c.isActived ? (
                   <input
                     type='text'
-                    class='form-control'
+                    className='form-control'
                     placeholder='Enter Promo Code'
+                    onChange={(e) => handlePromoCode(c.id, e.target.value)}
                   ></input>
                 ) : (
                   <input
                     disabled
                     type='text'
-                    class='form-control'
+                    className='form-control'
                     placeholder='Already active Promo Code'
                   ></input>
                 )}
                 <div className='ml-2'>
                   {c.isActived ? (
-                    <button class='btn btn-success disabled'>Activated</button>
+                    <button className='btn btn-success disabled'>
+                      Activated
+                    </button>
                   ) : (
-                    <button type='submit' class='btn btn-primary'>
+                    <button
+                      type='submit'
+                      className='btn btn-primary'
+                      onClick={(e) => handleActivate(c.id, e)}
+                    >
                       Activate
                     </button>
                   )}
@@ -69,10 +110,10 @@ export default function Coupons(props) {
         <h3 className=''>
           Coupons for user: {props.token.firstName} {props.token.lastName}
         </h3>
-        <form class='form-inline d-flex'>
+        <form className='form-inline d-flex'>
           <input
             type='text'
-            class='form-control'
+            className='form-control'
             placeholder='Search'
             onChange={(e) => setSearchText(e.target.value)}
           ></input>
@@ -81,9 +122,9 @@ export default function Coupons(props) {
         {couponsList}
       </>
     ) : (
-      <div class='d-flex justify-content-center'>
-        <div class='spinner-grow text-primary' role='status'></div>
-        <span class='sr-only'>Loading...</span>
+      <div className='d-flex justify-content-center'>
+        <div className='spinner-grow text-primary' role='status'></div>
+        <span className='sr-only'>Loading...</span>
       </div>
     );
 
